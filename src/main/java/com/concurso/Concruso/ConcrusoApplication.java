67a4adbd95ca77122f07437ed0f6ae3c90f3bc39
estebanortenzi <eortenzi@mobydigital.com>
2021-12-23 09:04:48 -0300

    Creacion del proyecto

---
+++ src/main/java/com/concurso/Concruso/ConcrusoApplication.java
@@ -0,0 +1,13 @@
+package com.concurso.Concruso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcrusoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcrusoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concurso/Concruso/ConcrusoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concurso.Concruso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcrusoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
